692432db4e459e5dbd466347a289b1b4cf4a4580
Edilson Nt <edilsonneto@outlook.com>
2014-12-03 05:11:20 -0300

    Game Speed Changed

--- src/spaceRider/GameEnvironment/SRPlayField.java
+++ src/spaceRider/GameEnvironment/SRPlayField.java
@@ -106,7 +106,7 @@ public class SRPlayField extends JPanel implements KeyListener, Runnable{
 	public void run() {
 		while(true){
 			try {
-				Thread.sleep(10);
+				Thread.sleep(9);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
